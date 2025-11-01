d852e67a1113071ca6923f8e2f33587d352824a0
Karetskii Vlad <kar.vladi@yandex.ru>
2025-11-01 03:42:14 +0300

    small fix

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/karetskiiVO/graphics-course
 go 1.23.2
 
 require (
-	github.com/jessevdk/go-flags v1.6.1 // indirect
-	github.com/qmuntal/gltf v0.28.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
+	github.com/jessevdk/go-flags v1.6.1
+	github.com/karetskiiVO/slices v0.0.0-20251031220617-63d18814a796
+	github.com/qmuntal/gltf v0.28.0
 )
+
+require golang.org/x/sys v0.21.0 // indirect
--- tasks/model_bakery/baker/model-baker/baker.go
+++ tasks/model_bakery/baker/model-baker/baker.go
@@ -3,17 +3,24 @@ package modelbaker
 import (
 	"bytes"
 	"encoding/binary"
-	"errors"
 	"fmt"
-	"math"
+	"io"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
-	"unsafe"
+	"sync"
 
+	"github.com/karetskiiVO/slices"
 	"github.com/qmuntal/gltf"
-	"github.com/qmuntal/gltf/modeler"
 )
+
+var i0 = sync.OnceValue(func() *int {
+	return gltf.Index(0)
+})
+var i1 = sync.OnceValue(func() *int {
+	return gltf.Index(1)
+})
 
 type Baker struct {
 }
@@ -28,24 +35,11 @@ func (b *Baker) BakeScene(scenepath string) error {
 		return fmt.Errorf("load: %w", err)
 	}
 
-	resultMeshes := b.processMeshes(doc)
-	b.updateExtensions(doc)
-
 	dir := filepath.Dir(scenepath)
 	base := strings.TrimSuffix(filepath.Base(scenepath), filepath.Ext(scenepath))
 	modelPath := filepath.Join(dir, base)
-	b.updateBuffer(modelPath, doc, &resultMeshes)
-	b.updateBufferView(doc, &resultMeshes)
 
-	var newAccessors []*gltf.Accessor
-	for i := range doc.Meshes {
-		mesh := doc.Meshes[i]
-		for j := range mesh.Primitives {
-			relem := &resultMeshes.Relems[resultMeshes.Meshes[i].FirstRelem+uint32(j)]
-			b.updateAccessors(&newAccessors, mesh.Primitives[j], relem)
-		}
-	}
-	doc.Accessors = newAccessors
+	b.optimize(doc)
 
 	outputPath := modelPath + "_baked.gltf"
 	if err := gltf.Save(doc, outputPath); err != nil {
@@ -64,346 +58,251 @@ func (Baker) load(scenepath string) (*gltf.Document, error) {
 	return gltf.Open(scenepath)
 }
 
-func (Baker) updateExtensions(doc *gltf.Document) {
-	doc.ExtensionsRequired = append(doc.ExtensionsRequired, "KHR_mesh_quantization")
-	doc.ExtensionsUsed = append(doc.ExtensionsUsed, "KHR_mesh_quantization")
-}
+func (b Baker) optimize(doc *gltf.Document) {
+	var result struct {
+		vertices  []CompressedVertex
+		indices   []uint32
+		accessors []*gltf.Accessor
+	}
 
-func (b *Baker) processMeshes(doc *gltf.Document) ProcessedMeshes {
-	var result ProcessedMeshes
+	newAccessor := func() *gltf.Accessor {
+		return new(gltf.Accessor)
+	}
+	floatUpscaler := func(x float32) float64 {
+		return float64(x)
+	}
 
-	warns := []error{}
 	for _, mesh := range doc.Meshes {
-		meshInfo := Mesh{
-			FirstRelem: uint32(len(result.Relems)),
-			RelemCount: uint32(len(mesh.Primitives)),
-		}
-
 		for _, prim := range mesh.Primitives {
-			if prim.Mode != gltf.PrimitiveTriangles {
-				warns = append(warns, fmt.Errorf("non-triangles primitive: %v", prim))
-				meshInfo.RelemCount--
-				continue
+			vertices, indices := b.acceptPrimitive(doc, prim)
+
+			prim.Indices = gltf.Index(len(result.accessors))
+			result.accessors = append(result.accessors, newAccessor())
+			idxAccessor := result.accessors[len(result.accessors)-1]
+			*idxAccessor = gltf.Accessor{
+				BufferView:    i0(),
+				ByteOffset:    binary.Size(result.indices),
+				Count:         len(indices),
+				ComponentType: gltf.ComponentUint,
+				Type:          gltf.AccessorScalar,
+
+				Min: []float64{float64(slices.Min(indices))},
+				Max: []float64{float64(slices.Max(indices))},
 			}
 
-			posIdx, hasPosition := prim.Attributes[gltf.POSITION]
-			normalIdx, hasNormals := prim.Attributes[gltf.NORMAL]
-			tangentIdx, hasTangents := prim.Attributes[gltf.TANGENT]
-			texcoordIdx, hasTexcoord := prim.Attributes["TEXCOORD_0"]
+			prim.Attributes["POSITION"] = len(result.accessors)
+			result.accessors = append(result.accessors, newAccessor())
+			posAccessor := result.accessors[len(result.accessors)-1]
+			*posAccessor = gltf.Accessor{
+				BufferView:    i1(),
+				ByteOffset:    binary.Size(result.vertices) + int(reflect.TypeFor[CompressedVertex]().Field(0).Offset),
+				Count:         len(vertices),
+				ComponentType: gltf.ComponentFloat,
+				Type:          gltf.AccessorVec3,
 
-			if !hasPosition || prim.Indices == nil {
-				warns = append(warns, fmt.Errorf("missing required POSITION or indices primitive: %v", prim))
-				meshInfo.RelemCount--
-				continue
+				Min: slices.Map(
+					slices.Reduce(
+						vertices[0].Position, vertices,
+						func(v Vec3f, elem Vertex) Vec3f { return *v.Min(v, elem.Position) },
+					).Slice(),
+					floatUpscaler,
+				),
+				Max: slices.Map(
+					slices.Reduce(
+						vertices[0].Position, vertices,
+						func(v Vec3f, elem Vertex) Vec3f { return *v.Max(v, elem.Position) },
+					).Slice(),
+					floatUpscaler,
+				),
 			}
 
-			indexAccessor := doc.Accessors[*prim.Indices]
-			posAccessor := doc.Accessors[posIdx]
-
-			relem := Element{
-				VertexOffset: uint32(len(result.Vertices)),
-				IndexOffset:  uint32(len(result.Indices)),
-				IndexCount:   uint32(indexAccessor.Count),
+			prim.Attributes["NORMAL"] = len(result.accessors)
+			result.accessors = append(result.accessors, newAccessor())
+			normAccessor := result.accessors[len(result.accessors)-1]
+			*normAccessor = gltf.Accessor{
+				BufferView:    i1(),
+				ByteOffset:    binary.Size(result.vertices) + int(reflect.TypeFor[CompressedVertex]().Field(1).Offset),
+				Count:         len(vertices),
+				ComponentType: gltf.ComponentByte,
+				Type:          gltf.AccessorVec3,
+				Normalized:    true,
 			}
 
-			vertexCount := posAccessor.Count
-
-			positions := make([][3]float32, vertexCount)
-			modeler.ReadPosition(doc, doc.Accessors[posIdx], positions)
-
-			var minPos, maxPos [3]float32
-			if len(positions) > 0 {
-				minPos = positions[0]
-				maxPos = positions[0]
-				for _, pos := range positions {
-					for i := range 3 {
-						if pos[i] < minPos[i] {
-							minPos[i] = pos[i]
-						}
-						if pos[i] > maxPos[i] {
-							maxPos[i] = pos[i]
-						}
-					}
-				}
+			prim.Attributes["TEXCOORD_0"] = len(result.accessors)
+			result.accessors = append(result.accessors, newAccessor())
+			texAccessor := result.accessors[len(result.accessors)-1]
+			*texAccessor = gltf.Accessor{
+				BufferView:    i1(),
+				ByteOffset:    binary.Size(result.vertices) + int(reflect.TypeFor[CompressedVertex]().Field(3).Offset),
+				Count:         len(vertices),
+				ComponentType: gltf.ComponentFloat,
+				Type:          gltf.AccessorVec2,
 			}
 
-			relem.MinPos = minPos
-			relem.MaxPos = maxPos
-
-			var normals [][3]float32
-			if hasNormals {
-				normals = make([][3]float32, vertexCount)
-				modeler.ReadNormal(doc, doc.Accessors[normalIdx], normals)
+			prim.Attributes["TANGENT"] = len(result.accessors)
+			result.accessors = append(result.accessors, newAccessor())
+			tangentAccessor := result.accessors[len(result.accessors)-1]
+			*tangentAccessor = gltf.Accessor{
+				BufferView:    i1(),
+				ByteOffset:    binary.Size(result.vertices) + int(reflect.TypeFor[CompressedVertex]().Field(4).Offset),
+				Count:         len(vertices),
+				ComponentType: gltf.ComponentByte,
+				Type:          gltf.AccessorVec4,
+				Normalized:    true,
 			}
 
-			var tangents [][4]float32
-			if hasTangents {
-				tangents = make([][4]float32, vertexCount)
-				modeler.ReadTangent(doc, doc.Accessors[tangentIdx], tangents)
-			}
-
-			var texcoords [][2]float32
-			if hasTexcoord {
-				texcoords = make([][2]float32, vertexCount)
-				modeler.ReadTextureCoord(doc, doc.Accessors[texcoordIdx], texcoords)
-			}
-
-			for i := 0; i < int(vertexCount); i++ {
-				var vtx Vertex
-				pos := positions[i]
-
-				var normal [3]float32
-				var tangent [3]float32
-				var texcoord [2]float32
-
-				if hasNormals {
-					normal = normals[i]
-				}
-				if hasTangents {
-					tangent = [3]float32{tangents[i][0], tangents[i][1], tangents[i][2]}
-				}
-				if hasTexcoord {
-					texcoord = texcoords[i]
-				}
-
-				encodedNormal := encodeNormal(NewVec4(normal[:]...))
-				encodedTangent := encodeNormal(NewVec4(tangent[:]...))
-
-				vtx.PositionAndNormal = NewVec4(pos[0], pos[1], pos[2], uint32ToFloat32(encodedNormal))
-				vtx.TexCoordAndTangentAndPadding = NewVec4(texcoord[0], texcoord[1], uint32ToFloat32(encodedTangent))
-
-				result.Vertices = append(result.Vertices, vtx)
-			}
-
-			indices := make([]uint32, indexAccessor.Count)
-			modeler.ReadIndices(doc, indexAccessor, indices)
-			result.Indices = append(result.Indices, indices...)
-
-			result.Relems = append(result.Relems, relem)
+			result.indices = append(result.indices, indices...)
+			result.vertices = append(result.vertices, slices.Map(vertices, compress)...)
 		}
-
-		result.Meshes = append(result.Meshes, meshInfo)
 	}
 
-	warn := errors.Join(warns...)
-	if warn != nil {
-		fmt.Fprintln(os.Stderr, warn)
+	buffer := bytes.NewBuffer(nil)
+	indexView := &gltf.BufferView{
+		Buffer:     0,
+		ByteOffset: buffer.Len(),
+		ByteLength: binary.Size(result.indices),
 	}
+	binary.Write(buffer, binary.LittleEndian, result.indices)
 
-	return result
-}
-
-func (b *Baker) updateBuffer(binPath string, doc *gltf.Document, resMeshes *ProcessedMeshes) {
-	data := bytes.NewBuffer(nil)
-
-	for _, idx := range resMeshes.Indices {
-		binary.Write(data, binary.LittleEndian, idx)
+	vertexView := &gltf.BufferView{
+		Buffer:     0,
+		ByteOffset: buffer.Len(),
+		ByteLength: binary.Size(result.vertices),
+		ByteStride: int(reflect.TypeFor[CompressedVertex]().Size()),
 	}
+	binary.Write(buffer, binary.LittleEndian, result.vertices)
 
-	for _, vtx := range resMeshes.Vertices {
-		binary.Write(data, binary.LittleEndian, vtx.PositionAndNormal)
-		binary.Write(data, binary.LittleEndian, vtx.TexCoordAndTangentAndPadding)
-	}
-
+	doc.Accessors = result.accessors
 	doc.Buffers = []*gltf.Buffer{{
-		Name:       filepath.Base(strings.TrimSuffix(binPath, filepath.Ext(binPath))),
-		URI:        filepath.Base(binPath) + "_baked.bin",
-		ByteLength: data.Len(),
-		Data:       data.Bytes(),
+		ByteLength: buffer.Len(),
+		Data:       buffer.Bytes(),
 	}}
+	doc.BufferViews = []*gltf.BufferView{indexView, vertexView}
+	doc.ExtensionsUsed = append(doc.ExtensionsUsed, "KHR_mesh_quantization")
+	doc.ExtensionsRequired = append(doc.ExtensionsRequired, "KHR_mesh_quantization")
 }
 
-func (b *Baker) updateBufferView(doc *gltf.Document, resMeshes *ProcessedMeshes) {
-	indsSize := len(resMeshes.Indices) * 4
-	vertsSize := len(resMeshes.Vertices) * int(unsafe.Sizeof(Vertex{}))
+func (Baker) acceptPrimitive(doc *gltf.Document, prim *gltf.Primitive) (vertices []Vertex, indices []uint32) {
+	const (
+		INDICES  = 0
+		POSITION = 1
+		NORMAL   = 2
+		TANGENT  = 3
+		TEXCOORD = 4
+	)
 
-	indsBufferView := &gltf.BufferView{
-		Buffer:     0,
-		ByteLength: indsSize,
-		Target:     gltf.TargetElementArrayBuffer,
+	if prim.Mode != gltf.PrimitiveTriangles {
+		fmt.Fprintln(os.Stderr, "non triangle mode, skip")
+		return []Vertex{}, []uint32{}
 	}
 
-	vertsBufferView := &gltf.BufferView{
-		Buffer:     0,
-		ByteOffset: indsSize,
-		ByteLength: vertsSize,
-		ByteStride: int(unsafe.Sizeof(Vertex{})),
-		Target:     gltf.TargetArrayBuffer,
+	accessorIDs := []any{
+		*prim.Indices,
+		"POSITION",
+		"NORMAL",
+		"TANGENT",
+		"TEXCOORD_0",
 	}
 
-	doc.BufferViews = []*gltf.BufferView{indsBufferView, vertsBufferView}
-}
-
-func (b *Baker) updateAccessors(newAccessors *[]*gltf.Accessor, prim *gltf.Primitive, relem *Element) {
-	_, hasNormals := prim.Attributes[gltf.NORMAL]
-	_, hasTangents := prim.Attributes[gltf.TANGENT]
-	_, hasTexcoord := prim.Attributes["TEXCOORD_0"]
-
-	prim.Attributes = make(gltf.PrimitiveAttributes)
-
-	indexOffset := int(relem.IndexOffset)
-	count := int(relem.IndexCount)
-	offset := int(relem.VertexOffset) * int(unsafe.Sizeof(Vertex{}))
-
-	indicesAccessor := &gltf.Accessor{
-		BufferView:    gltf.Index(0),
-		ByteOffset:    indexOffset * 4,
-		ComponentType: gltf.ComponentUint,
-		Count:         count,
-		Type:          gltf.AccessorScalar,
+	type Pipeline struct {
+		accessor *gltf.Accessor
+		bufView  *gltf.BufferView
+		r        io.Reader
+		stride   int
 	}
-	prim.Indices = gltf.Index(len(*newAccessors))
-	*newAccessors = append(*newAccessors, indicesAccessor)
 
-	posAccessor := &gltf.Accessor{
-		BufferView:    gltf.Index(1),
-		ByteOffset:    offset,
-		ComponentType: gltf.ComponentFloat,
-		Count:         count,
-		Type:          gltf.AccessorVec3,
-		Min:           []float64{float64(relem.MinPos[0]), float64(relem.MinPos[1]), float64(relem.MinPos[2])},
-		Max:           []float64{float64(relem.MaxPos[0]), float64(relem.MaxPos[1]), float64(relem.MaxPos[2])},
-	}
-	prim.Attributes[gltf.POSITION] = len(*newAccessors)
-	*newAccessors = append(*newAccessors, posAccessor)
-
-	if hasNormals {
-		normalAccessor := &gltf.Accessor{
-			BufferView:    gltf.Index(1),
-			ByteOffset:    offset + 12,
-			Normalized:    true,
-			ComponentType: gltf.ComponentByte,
-			Count:         count,
-			Type:          gltf.AccessorVec3,
+	assesorIdx := slices.Map(accessorIDs, func(v any) int {
+		switch vt := v.(type) {
+		case int:
+			return vt
+		case string:
+			res, ok := prim.Attributes[vt]
+			if !ok {
+				res = -1
+			}
+			return res
 		}
-		prim.Attributes[gltf.NORMAL] = len(*newAccessors)
-		*newAccessors = append(*newAccessors, normalAccessor)
-	}
+		return -1
+	})
 
-	if hasTexcoord {
-		texcoordAccessor := &gltf.Accessor{
-			BufferView:    gltf.Index(1),
-			ByteOffset:    offset + 16,
-			ComponentType: gltf.ComponentFloat,
-			Count:         count,
-			Type:          gltf.AccessorVec2,
-		}
-		prim.Attributes["TEXCOORD_0"] = len(*newAccessors)
-		*newAccessors = append(*newAccessors, texcoordAccessor)
-	}
-
-	if hasTangents {
-		tangentAccessor := &gltf.Accessor{
-			BufferView:    gltf.Index(1),
-			ByteOffset:    offset + 24,
-			Normalized:    true,
-			ComponentType: gltf.ComponentByte,
-			Count:         count,
-			Type:          gltf.AccessorVec4,
-		}
-		prim.Attributes[gltf.TANGENT] = len(*newAccessors)
-		*newAccessors = append(*newAccessors, tangentAccessor)
-	}
-}
-
-type Element struct {
-	VertexOffset uint32
-	IndexOffset  uint32
-	IndexCount   uint32
-	MinPos       [3]float32
-	MaxPos       [3]float32
-}
-
-type Mesh struct {
-	FirstRelem uint32
-	RelemCount uint32
-}
-
-type Vec4 struct {
-	X, Y, Z, W float32
-}
-
-func (v Vec4) Arr() [4]float32 {
-	return [4]float32{
-		v.X, v.Y, v.Z, v.W,
-	}
-}
-
-func NewVec4(coords ...float32) (res Vec4) {
-	switch len(coords) {
-	case 4:
-		res.W = coords[3]
-		fallthrough
-	case 3:
-		res.Z = coords[2]
-		fallthrough
-	case 2:
-		res.Y = coords[1]
-		fallthrough
-	case 1:
-		res.X = coords[0]
-		fallthrough
-	case 0:
-		return
-	default:
-		panic(fmt.Sprintf("len(coords:%v) > 4", coords))
-	}
-}
-
-type Vertex struct {
-	PositionAndNormal            Vec4
-	TexCoordAndTangentAndPadding Vec4
-}
-
-type ProcessedMeshes struct {
-	Vertices []Vertex
-	Indices  []uint32
-	Relems   []Element
-	Meshes   []Mesh
-}
-
-func encodeNormal(normal Vec4) uint32 {
-	// May be bug
-	bytes := [4]uint8{
-		uint8(math.Round(float64(normal.X * 127.0))),
-		uint8(math.Round(float64(normal.Y * 127.0))),
-		uint8(math.Round(float64(normal.Z * 127.0))),
-		uint8(math.Round(float64(normal.W * 127.0))),
-	}
-
-	return binary.LittleEndian.Uint32(bytes[:])
-}
-
-func uint32ToFloat32(u uint32) float32 {
-	return *(*float32)(unsafe.Pointer(&u))
-}
-
-func calculateMin(positions [][3]float32) []float32 {
-	if len(positions) == 0 {
-		return []float32{0, 0, 0}
-	}
-	min := positions[0]
-	for _, pos := range positions[1:] {
-		for i := 0; i < 3; i++ {
-			if pos[i] < min[i] {
-				min[i] = pos[i]
+	pipeline := slices.Map(assesorIdx, func(idx int) Pipeline {
+		if idx == -1 {
+			return Pipeline{
+				r: emptyReader{},
 			}
 		}
+
+		accessor := doc.Accessors[idx]
+		bufView := doc.BufferViews[*accessor.BufferView]
+		r := bytes.NewBuffer(doc.Buffers[bufView.Buffer].Data[bufView.ByteOffset+accessor.ByteOffset:])
+		stride := bufView.ByteStride
+		if stride == 0 {
+			stride = gltf.SizeOfElement(accessor.ComponentType, accessor.Type)
+		}
+
+		return Pipeline{
+			accessor: accessor,
+			bufView:  bufView,
+			r:        r,
+			stride:   stride,
+		}
+	})
+
+	remover := [128]byte{}
+	vertices = make([]Vertex, 0, pipeline[POSITION].accessor.Count)
+	for range pipeline[POSITION].accessor.Count {
+		v := Vertex{
+			Position: NewVec3[float32](),
+			Normal:   NewVec3[float32](0, 1, 0),
+			Tangent:  NewVec3[float32](1, 0, 0),
+			TexCoord: NewVec2[float32](),
+		}
+
+		slices.Map(
+			slices.Join(
+				[]int{POSITION, NORMAL, TANGENT, TEXCOORD},
+				[]any{&v.Position, &v.Normal, &v.Tangent, &v.TexCoord},
+			),
+			func(pair slices.Pair[int, any]) struct{} {
+				binary.Read(pipeline[pair.First].r, binary.LittleEndian, pair.Second)
+
+				overhead := max(0, pipeline[pair.First].stride-int(reflect.TypeOf(pair.Second).Elem().Size()))
+				pipeline[pair.First].r.Read(remover[:overhead])
+				return struct{}{}
+			},
+		)
+
+		vertices = append(vertices, v)
 	}
-	return min[:]
+
+	switch pipeline[INDICES].accessor.ComponentType {
+	case gltf.ComponentUshort:
+		indices = make([]uint32, 0, pipeline[INDICES].accessor.Count)
+		for range pipeline[INDICES].accessor.Count {
+			var buf uint16
+			binary.Read(pipeline[INDICES].r, binary.LittleEndian, &buf)
+			indices = append(indices, uint32(buf))
+		}
+	case gltf.ComponentUint:
+		indices = make([]uint32, pipeline[INDICES].accessor.Count)
+		binary.Read(pipeline[INDICES].r, binary.LittleEndian, &indices)
+	}
+	return vertices, indices
 }
 
-func calculateMax(positions [][3]float32) []float32 {
-	if len(positions) == 0 {
-		return []float32{0, 0, 0}
-	}
-	max := positions[0]
-	for _, pos := range positions[1:] {
-		for i := 0; i < 3; i++ {
-			if pos[i] > max[i] {
-				max[i] = pos[i]
-			}
+func compress(v Vertex) CompressedVertex {
+	compressfloat := func(v float32) int8 {
+		if v < -1 || 1 < v {
+			panic(fmt.Sprintf("overflow of float: %v", v))
 		}
+
+		return int8(v * 127)
 	}
-	return max[:]
+
+	return CompressedVertex{
+		Position: v.Position,
+		Norm:     NewVec3(slices.Map(v.Normal.Slice(), compressfloat)...),
+		TexCoord: v.TexCoord,
+		Tangent:  NewVec4(append(slices.Map(v.Tangent.Slice(), compressfloat), 127)...),
+	}
 }
